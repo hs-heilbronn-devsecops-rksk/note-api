a78e0d970bc07805934f4911d2a95bc098d3055f
unknown <kenil lathiya>
2025-01-16 13:52:13 +0100

    try4

--- note_api/main.py
+++ note_api/main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from uuid import uuid4
 from typing import List, Optional
 from os import getenv
@@ -14,12 +13,12 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.trace import get_tracer
 from opentelemetry import trace
 import uvicorn
 
 app = FastAPI()
 
+# OpenTelemetry Tracing
 resource = Resource.create({"service.name": "note-api"})
 tracer_provider = TracerProvider(resource=resource)
 cloud_trace_exporter = CloudTraceSpanExporter()
@@ -29,15 +28,12 @@ trace.set_tracer_provider(tracer_provider)
 
 FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
 
-tracer = get_tracer(__name__)
-
 my_backend: Optional[Backend] = None
 
 def get_backend() -> Backend:
-    global my_backend  # pylint: disable=global-statement
+    global my_backend
     if my_backend is None:
         backend_type = getenv('BACKEND', 'memory')
-        print(backend_type)
         if backend_type == 'redis':
             my_backend = RedisBackend()
         elif backend_type == 'gcs':
@@ -46,40 +42,27 @@ def get_backend() -> Backend:
             my_backend = MemoryBackend()
     return my_backend
 
-@app.get('/')
+@app.get("/")
 def redirect_to_notes() -> None:
-    return RedirectResponse(url='/notes')
+    return RedirectResponse(url="/notes")
 
-@app.get('/notes')
+@app.get("/notes")
 def get_notes(backend: Annotated[Backend, Depends(get_backend)]) -> List[Note]:
-    with tracer.start_as_current_span("get_notes_span"):
-        keys = backend.keys()
+    return [backend.get(key) for key in backend.keys()]
 
-        Notes = []
-        for key in keys:
-            Notes.append(backend.get(key))
-        return Notes
+@app.get("/notes/{note_id}")
+def get_note(note_id: str, backend: Annotated[Backend, Depends(get_backend)]) -> Note:
+    return backend.get(note_id)
 
-@app.get('/notes/{note_id}')
-def get_note(note_id: str,
-             backend: Annotated[Backend, Depends(get_backend)]) -> Note:
-    with tracer.start_as_current_span("get_note_span"):
-        return backend.get(note_id)
+@app.put("/notes/{note_id}")
+def update_note(note_id: str, request: CreateNoteRequest, backend: Annotated[Backend, Depends(get_backend)]) -> None:
+    backend.set(note_id, request)
 
-@app.put('/notes/{note_id}')
-def update_note(note_id: str,
-                request: CreateNoteRequest,
-                backend: Annotated[Backend, Depends(get_backend)]) -> None:
-    with tracer.start_as_current_span("update_note_span"):
-        backend.set(note_id, request)
-
-@app.post('/notes')
-def create_note(request: CreateNoteRequest,
-                backend: Annotated[Backend, Depends(get_backend)]) -> str:
-    with tracer.start_as_current_span("create_note_span"):
-        note_id = str(uuid4())
-        backend.set(note_id, request)
-        return note_id
+@app.post("/notes")
+def create_note(request: CreateNoteRequest, backend: Annotated[Backend, Depends(get_backend)]) -> str:
+    note_id = str(uuid4())
+    backend.set(note_id, request)
+    return note_id
 
 if __name__ == "__main__":
     port = int(getenv("PORT", 8080))
